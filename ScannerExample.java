e863c12f00d2f81ca034d12c903f08dbd2583c0c
Haneul0112 <khg36299@naver.com>
2025-05-08 12:00:00 +0900

    Scanner濡??낅젰 諛쏄린 ?곗뒿

---
+++ ScannerExample.java
@@ -0,0 +1,1 @@
+import java.util.Scanner; public class ScannerExample { public static void main(String[] args) { Scanner sc = new Scanner(System.in); System.out.print("?´ë¦„ ?…ë ¥: "); String name = sc.nextLine(); System.out.println("?ˆë…•?˜ì„¸??" + name); } }
